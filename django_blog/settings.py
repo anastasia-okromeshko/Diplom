0a73d777a2d9f0554e4571d9f20d5218aeb156a8
anastasia-okromeshko <nastya_yankovich@mail.ru>
2021-07-08 08:12:45 +0300

    two commit

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -73,6 +73,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '8000',
     }
 }
 
